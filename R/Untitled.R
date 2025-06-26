59e9e5728bbce4557c87b519619ea81d133e113d
XiQiao2023 <kathyqiao87@gmail.com>
2025-06-26 16:00:53 -0600

    6_26

---
+++ R/Untitled.R
@@ -0,0 +1,13 @@
+#' Add Two Numbers
+#'
+#' This function adds two numbers together.
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#' @return The sum of x and y.
+#' @examples
+#' add_two(2, 3)
+#' @export
+add_two <- function(x, y) {
+  x + y
+}
